f58a34923a70cf74966efa897bcdf7be3145a11b
Айша <aisha@192.168.0.5>
2025-09-24 00:42:03 +0500

    homework2

---
+++ my-app/src/AboutMe.jsx
@@ -0,0 +1,39 @@
+
+import "./AboutMe.css";
+
+function AboutMe() {
+  return (
+    <section className="about-details section-padding">
+      <div className="container">
+        <div className="row">
+          <div className="col col-left">
+            <div className="single_about_details">
+              <h3>About Me</h3>
+              <span></span>
+              <h2 className="mb20">
+                My name is Aisha. I am 20 years old.
+                <br />
+                4th year student at KBTU.
+              </h2>
+              <h4>
+                What do I like?
+                <br />
+                To please my loved ones, to read books, to celebrate the dawn,
+                to cook with my friends, to travel with my family.
+              </h4>
+              <h4>
+                My contacts:
+                <br />
+                E-mail:ai_abilmazhinova@kbtu.kz
+                <br />
+                Telegram: @abilmazh1nova
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
